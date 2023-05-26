d09b51f21d614a64b6988bd6dee9d12f0e659a77
first-impact <134684336+first-impact@users.noreply.github.com>
2023-05-26 16:09:22 +0800

    Create bar_plot.py

---
+++ bar_plot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = ['A', 'B', 'C', 'D']
+y = [3, 7, 4, 9]
+
+# Create a bar plot
+plt.bar(x, y)
+
+# Set the labels and title
+plt.xlabel('X label')
+plt.ylabel('Y label')
+plt.title('Bar plot')
+
+# Show the plot
+plt.show()
