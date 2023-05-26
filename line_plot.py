ae3d5dc97da6bdee38cc27c354dd670aab467113
first-impact <134684336+first-impact@users.noreply.github.com>
2023-05-26 16:08:37 +0800

    Create line_plot.py

---
+++ line_plot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4]
+y = [1, 4, 9, 16]
+
+# Create a line plot
+plt.plot(x, y)
+
+# Set the labels and title
+plt.xlabel('X label')
+plt.ylabel('Y label')
+plt.title('Line plot')
+
+# Show the plot
+plt.show()
