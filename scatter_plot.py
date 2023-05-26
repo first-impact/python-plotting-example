e004b5a2af95953eba1c2ebf1062ff67a5f440c7
first-impact <134684336+first-impact@users.noreply.github.com>
2023-05-26 16:09:00 +0800

    Create scatter_plot.py

---
+++ scatter_plot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4]
+y = [1, 4, 9, 16]
+
+# Create a scatter plot
+plt.scatter(x, y)
+
+# Set the labels and title
+plt.xlabel('X label')
+plt.ylabel('Y label')
+plt.title('Scatter plot')
+
+# Show the plot
+plt.show()
